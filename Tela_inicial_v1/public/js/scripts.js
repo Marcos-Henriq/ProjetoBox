a9a12b7590d9f03a65b6ea4f79c849c9ef07e3db
Guilherme Ferreira Gonçalves <guilherme.goncalves@BD0724.boxdelivery.local>
2022-07-28 13:29:13 -0300

    sistema de volta de tela

--- Tela_inicial_v1/public/js/scripts.js
+++ Tela_inicial_v1/public/js/scripts.js
@@ -1,21 +1,53 @@
 
+    var anterior=''
+    var atual=''
+    var caminho=[]
 function troca(id, aparecer) {
+    anterior='inicial'
+    atual=aparecer
     if (aparecer == 'fim') {
         window.location = 'formulario'
-
     }
 
     document.getElementById(id).style.display = "none"
     document.getElementById(aparecer).style.display = "flex"
-
-    let caminho = [];
     caminho.push(aparecer);
-    if (caminho.length > 0) {
+    if (caminho.length > 0) {   
         document.getElementById('linkHome').style.display = 'flex'
     }
-    for (let i = 0; i < caminho.length; i++) {
+    
 
-        document.getElementById('containerRoute').innerHTML += `<a class="link-route" href="#${caminho[i]}">${caminho[i]}</a>`
-    }
-    console.log(caminho)
+
+
+    // Implementação futura da volta com caminho de pão
+
+
+
+    // document.getElementById('containerRoute').innerHTML = ``
+    // for (let i = 0; i < caminho.length; i++) {
+    //     document.getElementById('containerRoute').innerHTML += `<button  onclick="voltar(${i})" class="link-route">${caminho[i]}</button><br>`
+        
+    //     /* if (i==0){
+    //         document.getElementById('containerRoute').innerHTML += `<button  onclick="voltar(0)" class="link-route">${caminho[i]}</button><br>`
+    //     }
+    //     if (i==1){
+    //         document.getElementById('containerRoute').innerHTML += `<button class="link-route" onclick="voltar(1)">${caminho[i]}</button><br>`
+    //     }
+    //     if (i==2){
+    //         document.getElementById('containerRoute').innerHTML += `<button class="link-route" onclick="voltar(2)" >${caminho[i]}</button><br>`
+    //     }
+    //        */  
+
+    // }
 }   
+
+function voltar(){
+    caminho=[]
+    document.getElementById(atual).style.display = "none"
+    document.getElementById(anterior).style.display = "flex"
+    if(document.getElementById(anterior).style.display == "flex"){
+        document.getElementById('linkHome').style.display = 'none'
+    }
+}
+
+
